9deccea575a5d707bcfa35d8ebc6a5ded738066c
chrisfernandes102 <christopherjoseph.fernandes@calbaptist.edu>
2019-03-30 22:09:08 -0700

    Added json file to search xml data

--- src/JavaServer.java
+++ src/JavaServer.java
@@ -1,4 +1,10 @@
 import helma.xmlrpc.WebServer;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 // Description : Get message from client, and display it on the screen
 public class JavaServer {
@@ -17,8 +23,29 @@ public class JavaServer {
     }
 
     // Method to get Message (String) from client
-    public String pesanClient(String pesan) {
+    public String pesanClient(String pesan) throws FileNotFoundException {
         System.out.println(pesan);
-        return "Client message displayed";
+
+        String verse = null;
+
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object obj = parser.parse(new FileReader("bible.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            verse = (String) jsonObject.get(pesan);
+            System.out.println(verse);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
+        return verse;
     }
 }
